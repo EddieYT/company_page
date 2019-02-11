fcf3c3b7f5c3adeca11d4acf1538260e2a28fb08
EddieYT <yingt@seas.upenn.edu>
2019-02-10 23:51:53 -0500

    Brief Update

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ window.addEventListener('scroll', function() {
   }
 });
 
-Smooth Scrolling
+// Smooth Scrolling
 $('#navbar a, .btn').on('click', function(event) {
   if (this.hash !== '') {
     event.preventDefault();
